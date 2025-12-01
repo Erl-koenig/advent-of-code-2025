d1121f5d476fa4594299e22ec8c1769532c1c442
Ivan Knoefler <68387388+Erl-koenig@users.noreply.github.com>
2025-12-01 15:36:29 +0100

    day1

---
+++ day1.py
@@ -0,0 +1,33 @@
+from utils import load_input
+
+DIAL_SIZE = 100
+idx = 50
+data = load_input(1)
+part1 = 0
+part2 = 0
+
+for line in data:
+    direction = line[0]
+    dist = int(line[1:])
+
+    # part 2
+    if direction == "R":
+        first = (DIAL_SIZE - idx) % DIAL_SIZE or DIAL_SIZE
+    else:
+        first = idx if idx > 0 else DIAL_SIZE
+
+    if dist >= first:
+        part2 += 1 + (dist - first) // DIAL_SIZE
+
+    # update pos
+    if direction == "R":
+        idx = (idx + dist) % DIAL_SIZE
+    else:
+        idx = (idx - dist) % DIAL_SIZE
+
+    # part 1
+    if idx == 0:
+        part1 += 1
+
+print("part1: ", part1)
+print("part2: ", part2)
---
+++ utils.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+def load_input(day):
+    filename = f"input/day{day:02}.txt"
+    path = Path(filename)
+    return path.read_text().strip().splitlines()
